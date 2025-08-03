6e6c6c8564af59ce707a574281efb37f199eaf00
Mohammed-Mshal <83920083+Mohammed-Mshal@users.noreply.github.com>
2025-08-03 15:43:23 +0300

    Upadte Home

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    // banner home
+    gsap.registerPlugin(ScrollTrigger)
+    const socialLinks=document.querySelectorAll('.home-banner .list-social a')
+    const tlSocial=gsap.timeline()
+    socialLinks.forEach((el,i)=>{
+        tlSocial.from((el),{
+            scale:1.5,
+            opacity:0,
+            delay:i===0?1:0,
+            duration:.4,
+            ease:'power2.inOut'
+        })
+    })
     const bannerHome = document.querySelector('.home-banner .swiper-home')
     if (bannerHome) {
         const swiperHome = new Swiper(bannerHome, {
             slidesPerView: 1,
+            loop: true,
+            // autoplay: {
+            //     delay: 3000,
+            //     disableOnInteraction: false,
+            // },
+            pagination: {
+                el: '.swiper-pagination',
+            },
         })
     }
 })
